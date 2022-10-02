0ae775658cac40813e83d84244ca2d00adc1e21b
chmuhammadasim <boybrave82@gmail.com>
2022-10-02 20:49:44 +0500

    Create extraF.h

---
+++ array/extraF.h
@@ -0,0 +1,119 @@
+#ifndef extra_H
+#define extra_H
+#include<string.h>
+#include<iostream>
+using namespace std;
+void dbmsintro(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n|                      Welcome To                     |";
+    cout << "\n|               Array Management System               |";
+    cout << "\n|           By Muhammad Asim Chattha (210970)         |\n";
+    cout << "|=====================================================|\n\n";
+    system("pause");
+    system("cls");
+}
+void dbmsmenu(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n                !!Select Operation!!                   ";
+    cout << "\n            |        1--Insert       |                 ";
+    cout << "\n            |        2--Display      |                 ";
+    cout << "\n            |        3--Search       |                 ";
+    cout << "\n            |        4--Update       |                 ";
+    cout << "\n            |        5--Delete       |                 ";
+    cout << "\n            |        6--Swap         |                 ";
+    cout << "\n            |        7--Exit         |                 ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void number(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n                !! Enter Number !!                   ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tNumber : ";
+}
+void updatenumber(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n             !! Enter Update Number !!               ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tNumber : ";
+}
+void index(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n                !! Enter Index  !!                   ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tNumber : ";
+}
+void loca(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n               !! Enter Location !!                  ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tLocation : ";
+}
+void case1(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n                !!Select Option(ADD)!!                   ";
+    cout << "\n            |        1--Start           |              ";
+    cout << "\n            |        2--Specfic         |              ";
+    cout << "\n            |        3--End             |              ";
+    cout << "\n            |        4--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void case2(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n              !!Select Option(Search)!!                ";
+    cout << "\n            |        1--Number          |              ";
+    cout << "\n            |        2--Index           |              ";
+    cout << "\n            |        3--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void case3(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n              !!Select Option(Update)!!                ";
+    cout << "\n            |        1--Number          |              ";
+    cout << "\n            |        2--Index           |              ";
+    cout << "\n            |        3--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void case4(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n              !!Select Option(Delete)!!                ";
+    cout << "\n            |        1--Number          |              ";
+    cout << "\n            |        2--Index           |              ";
+    cout << "\n            |        3--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void case5(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n               !!Select Option(Swap)!!                 ";
+    cout << "\n            |        1--Number          |              ";
+    cout << "\n            |        2--Index           |              ";
+    cout << "\n            |        3--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void case6(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n               !!Select Option(Copy)!!                 ";
+    cout << "\n            |        1--Number          |              ";
+    cout << "\n            |        2--Index           |              ";
+    cout << "\n            |        3--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+#endif
