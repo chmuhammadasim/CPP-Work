d3ef68dc86b5ac2bf20babc27cdd9d87a8644a58
chmuhammadasim <boybrave82@gmail.com>
2022-10-18 01:10:26 +0500

    Create extraF.h

---
+++ link_list/extraF.h
@@ -0,0 +1,87 @@
+#ifndef extra_H
+#define extra_H
+#include<string.h>
+#include<iostream>
+#include<fstream>
+using namespace std;
+void dbmsintro(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n|                      Welcome To                     |";
+    cout << "\n|              link list Management System            |";
+    cout << "\n|           By Muhammad Asim Chattha (210970)         |\n";
+    cout << "|=====================================================|\n\n";
+    system("pause");
+    system("cls");
+}
+void dbmsmenu(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n                !!Select Operation!!                   ";
+    cout << "\n            |        1--Add          |                 ";
+    cout << "\n            |        2--Display Data |                 ";
+    cout << "\n            |        3--List lenght  |                 ";
+    cout << "\n            |        4--Delete       |                 ";
+    cout << "\n            |        5--Update       |                 ";
+    cout << "\n            |        6--Swap         |                 ";
+    cout << "\n            |        7--Copy         |                 ";
+    cout << "\n            |        8--Exit         |                 ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void number(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n              !!Enter  Number!!                   ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tNumber : ";
+}
+void number2(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n              !!Enter  Number(2)!!                 ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tNumber : ";
+}
+
+void location(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n              !!Enter  Location!!                   ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tLocation : ";
+}
+void choice1(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n                !!Select Option(ADD)!!                   ";
+    cout << "\n            |        1--Start           |              ";
+    cout << "\n            |        2--Specfic         |              ";
+    cout << "\n            |        3--End             |              ";
+    cout << "\n            |        4--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void choice2(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n              !!Select Option(Delete)!!                   ";
+    cout << "\n            |        1--Start           |              ";
+    cout << "\n            |        2--Specfic         |              ";
+    cout << "\n            |        3--End             |              ";
+    cout << "\n            |        4--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+void choice3(){
+    system("cls");
+    cout << "|=====================================================|";
+    cout << "\n              !!Select Option(Update)!!                   ";
+    cout << "\n            |        1--Start           |              ";
+    cout << "\n            |        2--Specfic         |              ";
+    cout << "\n            |        3--End             |              ";
+    cout << "\n            |        4--Main Menu       |              ";
+    cout << "\n|=====================================================|\n\n\n\n";
+    cout<<"\tOption : ";
+}
+#endif
